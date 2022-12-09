c78dfcbd2986c3c04c22c7291569dfe15d03b604
Susindhar-M <suriyamanivasagan12@gmail.com>
2022-12-09 21:14:30 +0530

    Uploading day 8 program

---
+++ Caesar-cypher.py
@@ -0,0 +1,79 @@
+#Author : Susindhar M
+#Date   : 09-12-2022
+#Caesar-Cypher
+
+
+#Encode function definition
+def encode():
+
+	#Variable initializations
+	e_code=""
+	alphabet_NC=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	message = input("\nEnter your Message Here :").lower()
+	key = int(input("Enter the number of shifts : "))
+
+	#Shifted alphabet list generation
+	for j in range (0,key):
+		for i in range (0,25):
+			temp = alphabet[i]
+			alphabet[i] = alphabet[i+1]
+			alphabet[i+1] = temp
+
+	#code word generation
+	for i in range(0,len(message)):
+		for j in range(0,25):
+			if alphabet_NC[j]==message[i]:
+				e_code+=alphabet[j]
+	
+	#Printing out result
+	print(e_code)
+
+#decode function definition
+def decode():
+
+	#variable initialization
+	d_code=""
+	alphabet_NC=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	message = input("\nEnter your Message Here :").lower()
+	key = int(input("Enter the number of shifts : "))
+	
+	#Shifted alphabet list generation
+	for j in range (0,key):
+		for i in range (25,0,-1):
+			temp = alphabet[i-1]
+			alphabet[i-1] = alphabet[i]
+			alphabet[i] = temp
+
+	#code word generation
+	for i in range(0,len(message)):
+		for j in range(0,25):
+			if alphabet_NC[j]==message[i]:
+				d_code+=alphabet[j]
+	
+	#Printing out result
+	print(d_code)
+
+#Main Code			
+run = True
+print("\nWelcome to caesar Cypher!\n")
+
+#Main Loop
+while run==True:			
+	func = input("Type 'Encode' to encrypt or 'decode' to decrypt : ").lower()
+	#Function selection
+	if func=="encode":
+		encode()
+	elif func=="decode":
+		decode()
+	else:
+		print("Not Valid :(")
+
+	cont = input("Type 'Yes' to Continue or 'No' to Exit : ").lower()	
+	#Loop decision
+	if cont=="no":
+		run=False
+	
+	
+	
